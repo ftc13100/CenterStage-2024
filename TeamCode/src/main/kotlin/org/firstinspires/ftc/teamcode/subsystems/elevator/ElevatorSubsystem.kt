1756de7bea9d4940ce81347066dc739881886011
SachetK <71898487+SachetK@users.noreply.github.com>
2023-10-13 19:02:01 -0400

    Create framework for PID Elevator and Motion Profiled

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/elevator/ElevatorSubsystem.kt
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.subsystems.elevator
+
+import com.arcrobotics.ftclib.controller.PIDFController
+import com.arcrobotics.ftclib.hardware.motors.Motor
+import org.firstinspires.ftc.teamcode.utils.PIDSubsystem
+
+class PIDElevatorSubsystem(
+    private val elevatorMotor: Motor
+) : PIDSubsystem(
+    PIDFController(
+        0.0,
+        0.0,
+        0.0,
+        0.0
+    )
+) {
+    override fun useOutput(output: Double, setpoint: Double) = elevatorMotor.set(output)
+
+    override fun getMeasurement(): Double = elevatorMotor.currentPosition.toDouble()
+
+
+}
