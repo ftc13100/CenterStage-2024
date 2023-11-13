a0397ded3d3e4eb3e091d52cb3f96436299e2822
Sachet Korada <sachet.korada@gmail.com>
2023-11-12 19:49:18 -0500

    Flip outtake

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/elevator/ElevatorSubsystem.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/elevator/ElevatorSubsystem.kt
@@ -2,11 +2,15 @@ package org.firstinspires.ftc.teamcode.subsystems.elevator
 import com.arcrobotics.ftclib.controller.PIDFController
 import com.arcrobotics.ftclib.hardware.motors.Motor
 import com.arcrobotics.ftclib.hardware.motors.MotorGroup
+import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.utils.PIDSubsystem
+import org.firstinspires.ftc.teamcode.utils.ServoGroup
 
 class ElevatorSubsystem(
     elevatorLeft: Motor,
-    elevatorRight: Motor
+    elevatorRight: Motor,
+    servoLeft: Servo,
+    servoRight: Servo
 ) : PIDSubsystem(
     PIDFController(
         0.0,
@@ -16,8 +20,13 @@ class ElevatorSubsystem(
     )
 ) {
     private val elevatorMotors = MotorGroup(elevatorLeft, elevatorRight)
+    private val elevatorServos = ServoGroup(servoLeft, servoRight)
+
+    private val flipped = elevatorServos.position == 1.0
+
     override fun useOutput(output: Double, setpoint: Double) = elevatorMotors.set(output)
 
     override fun getMeasurement(): Double = elevatorMotors.currentPosition.toDouble()
 
+    fun flipOuttake() = if (flipped) elevatorServos.position = 0.0 else elevatorServos.position = 0.0
 }
---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/utils/ServoGroup.kt
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.utils
+
+import com.qualcomm.robotcore.hardware.Servo
+
+class ServoGroup(
+    private vararg val servos : Servo
+) {
+    var position
+        get() = servos[0].position
+        set(position) {
+            servos.forEach { it.position = position }
+        }
+}
