93142e3f27c8a521880c9b9c8683532cfac2e523
SachetK <71898487+SachetK@users.noreply.github.com>
2023-11-15 14:54:41 -0500

    Auto start poses

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/constants/AutoStartPose.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/constants/AutoStartPose.kt
@@ -3,8 +3,27 @@ package org.firstinspires.ftc.teamcode.constants
 import com.acmerobotics.roadrunner.geometry.Pose2d
 
 enum class AutoStartPose(val startPose: Pose2d) {
-    RED_LEFT(Pose2d()),
-    RED_RIGHT(Pose2d()),
-    BLUE_LEFT(Pose2d()),
-    BLUE_RIGHT(Pose2d()),
+    RED_LEFT(
+        Pose2d(
+            -36.0, -61.5, Math.toRadians(-90.0)
+        )
+    ),
+
+    RED_RIGHT(
+        Pose2d(
+            10.0, -61.5, Math.toRadians(90.0)
+        )
+    ),
+
+    BLUE_LEFT(
+        Pose2d(
+            10.0, 61.5, Math.toRadians(-90.0)
+        )
+    ),
+
+    BLUE_RIGHT(
+        Pose2d(
+            -36.0, 61.5, Math.toRadians(-90.0)
+        )
+    ),
 }
